a5bf82e9de53bba150e99a8e6a397310aa92a092
David R. Andersen <k0rx@RXcomm.net>
2013-10-04 17:21:21 -0500

    changed setup.py

--- setup.py
+++ setup.py
@@ -1,12 +1,30 @@
-import subprocess
 from distutils.core import setup
+import zipfile
+import os
+import pwd
 
-#setup(name='encoDHer',
-#      version='0.1',
-#      description='symmetric email utility for python',
-#      author='David R. Andersen',
-#      url='https://github.com/rxcomm/encoDHer',
-#     py_modules=['encodher','dhutils','dh','constants','hsub'],
-#     )
+setup(name='encoDHer',
+      version='0.1',
+      description='symmetric email encryption utility for python',
+      author='David R. Andersen',
+      url='https://github.com/rxcomm/encoDHer',
+     py_modules=['encodher','dhutils','dh','constants','hsub'],
+     )
 
-subprocess.call('./make_exec.sh')
+
+with zipfile.ZipFile('encodher', 'w', zipfile.ZIP_DEFLATED) as z:
+    z.write('__main__.py')
+    z.write('encodher.py')
+    z.write('dhutils.py')
+    z.write('dh.py')
+    z.write('hsub.py')
+
+with open('encodher', 'r+') as z:
+    zipdata = z.read()
+    z.seek(0)
+    z.write('#!/usr/bin/env python\n'+zipdata)
+
+user = os.getenv('SUDO_USER')
+uid, gid = pwd.getpwnam(user)[2:4]
+os.chown('encodher', uid, gid)
+os.chmod('encodher',0755)
